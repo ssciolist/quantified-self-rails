df64d7dbf6fb821def0f80dbcb4e057f9a78f4f4
Megan Arellano <ssciolist@gmail.com>
2018-06-25 20:33:54 -0600

    adds shoulda matchers config and db cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,40 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start "rails" do
+  add_filter "app/channels/application_cable/channel.rb"
+  add_filter "app/channels/application_cable/connection.rb"
+  add_filter "app/jobs/application_job.rb"
+  add_filter "app/helpers/application_helper.rb"
+  add_filter "app/models/application_record.rb"
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+DatabaseCleaner.strategy = :truncation
+
+  RSpec.configure do |c|
+    c.before(:each) do
+      DatabaseCleaner.clean
+    end
+
+    c.after(:each) do
+      DatabaseCleaner.clean
+    end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
