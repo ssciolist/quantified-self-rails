9b7370c4b59bd6f928e619dd8d949ddb41e320e0
Megan Arellano <ssciolist@gmail.com>
2018-06-27 08:48:34 -0600

    slightly better meals test

--- spec/requests/api/v1/meals/all_meals_request_spec.rb
+++ spec/requests/api/v1/meals/all_meals_request_spec.rb
@@ -4,14 +4,19 @@ describe 'Meals API' do
   context 'when I send a GET request to /api/v1/meals' do
     it 'all meals and their foods in the database currently are returned' do
       create(:meal)
-      
+
       create_list(:food, 3)
 
       get '/api/v1/meals'
 
       expect(response).to be_successful
 
+      meals = JSON.parse(response.body)
+      first_meal = meals[0]
 
+      expect(first_meal).to have_key('id')
+      expect(first_meal).to have_key('name')
+      expect(first_meal).to have_key('foods')
     end
   end
 end
