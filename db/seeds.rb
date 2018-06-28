95a3c4d775a91cd484fad6826329b767cefcbb4b
Megan Arellano <ssciolist@gmail.com>
2018-06-27 18:29:00 -0600

    writes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Meal.create(name: 'Breakfast')
+Meal.create(name: 'Snacks')
+Meal.create(name: 'Lunch')
+Meal.create(name: 'Dinner')
